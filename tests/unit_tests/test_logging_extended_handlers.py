52ddec82458539d95f324dacda83fddd6193c65e
mbirke <wesci@web.de>
2025-03-23 12:16:42 +0100

    unit tests: with dependencies

--- tests/unit_tests/test_logging_extended_handlers.py
+++ tests/unit_tests/test_logging_extended_handlers.py
@@ -6,6 +6,7 @@ from pytest_socket import SocketBlockedError
 from logging_extended_handlers import BufferingSMTPHandler, HTTPHandlerCustomHeader
 
 
+@pytest.mark.dependency(name="ensure_socket_blocked")
 def test_socket_blocked(fixt_test_host):
     connection = http.client.HTTPConnection(host=fixt_test_host)
 
@@ -13,9 +14,11 @@ def test_socket_blocked(fixt_test_host):
         connection.request("GET", "/")
 
 
+@pytest.mark.dependency(depends=["ensure_socket_blocked"])
 def test_smtp_handler_init():
     BufferingSMTPHandler("myhost", "sender@com.com", ["recp@com.com"], "test", 999)
 
 
+@pytest.mark.dependency(depends=["ensure_socket_blocked"])
 def test_http_handler_init():
     HTTPHandlerCustomHeader("bin657rfgdiol123.com", "", header_key_value_pairs=[("k1", "v1")])
